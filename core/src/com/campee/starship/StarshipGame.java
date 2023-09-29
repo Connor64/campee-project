b6c6f3e5fae569311fe211f8815eb32c5a359975
evelyn <merze@purdue.edu>
2023-09-29 17:53:16 -0400

    new teal

--- core/src/com/campee/starship/StarshipGame.java
+++ core/src/com/campee/starship/StarshipGame.java
@@ -28,7 +28,7 @@ public class StarshipGame extends ApplicationAdapter {
 
 	@Override
 	public void render () {
-		ScreenUtils.clear(0, 50, 100, 1);
+		ScreenUtils.clear(0, 100, 100, 1);
 		batch.begin();
 		batch.draw(img, 400 - (img.getWidth() / 2), 300 - (img.getHeight() / 2));
 		batch.end();
