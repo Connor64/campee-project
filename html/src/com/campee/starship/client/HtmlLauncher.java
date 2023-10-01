722366935fa584dc0d6c1deaceba56be808bd076
protimab <protimabanerjee@gmail.com>
2023-09-30 22:56:39 -0400

    created classes

--- html/src/com/campee/starship/client/HtmlLauncher.java
+++ html/src/com/campee/starship/client/HtmlLauncher.java
@@ -3,7 +3,7 @@ package com.campee.starship.client;
 import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.backends.gwt.GwtApplication;
 import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
-import com.campee.starship.StarshipGame;
+import com.campee.starship.MoonshipGame;
 
 public class HtmlLauncher extends GwtApplication {
 
@@ -17,6 +17,6 @@ public class HtmlLauncher extends GwtApplication {
 
         @Override
         public ApplicationListener createApplicationListener () {
-                return new StarshipGame();
+                return new MoonshipGame();
         }
 }
