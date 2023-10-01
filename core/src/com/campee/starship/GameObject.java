bb487fda85de047d00f1616eabf16e70dc99a1bb
protimab <protimabanerjee@gmail.com>
2023-09-30 23:26:08 -0400

    made some changes to Sprite class

--- core/src/com/campee/starship/GameObject.java
+++ core/src/com/campee/starship/GameObject.java
@@ -1,4 +1,16 @@
 package com.campee.starship;
 
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Pixmap;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector;
+import com.badlogic.gdx.math.Vector2;
+
 public class GameObject {
+    Vector2 position;
+    boolean visible;
+    CustomSprite sprite;
+
 }
