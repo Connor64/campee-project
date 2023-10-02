db0dcd5758b2636040501a9c02afe6b69ae8f1b8
evelyn <merze@purdue.edu>
2023-10-01 21:35:00 -0400

    made a practice game and messed with images and clicking issues

--- core/src/com/campee/starship/MoonshipGame.java
+++ core/src/com/campee/starship/MoonshipGame.java
@@ -2,20 +2,24 @@ package com.campee.starship;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Pixmap;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MoonshipGame extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
-	
+
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		Pixmap pix_big = new Pixmap(Gdx.files.internal("connor_apple.jpg"));
-		Pixmap pix_small = new Pixmap(500, 100, pix_big.getFormat());
+		Pixmap pix_small = new Pixmap(400, 400, pix_big.getFormat());
 		pix_small.drawPixmap(pix_big,
 				0, 0, pix_big.getWidth(), pix_big.getHeight(),
 				0, 0, pix_small.getWidth(), pix_small.getHeight()
@@ -33,7 +37,7 @@ public class MoonshipGame extends ApplicationAdapter {
 		batch.draw(img, 400 - (img.getWidth() / 2), 300 - (img.getHeight() / 2));
 		batch.end();
 	}
-	
+
 	@Override
 	public void dispose () {
 		batch.dispose();
