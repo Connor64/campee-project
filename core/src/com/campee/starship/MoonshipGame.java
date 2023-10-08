0c711a15f5b5a3d181394231226b8f7f0b2570c3
evelyn <merze@purdue.edu>
2023-10-08 00:19:46 -0400

    test implementing different screens from input listener

--- core/src/com/campee/starship/MoonshipGame.java
+++ core/src/com/campee/starship/MoonshipGame.java
@@ -1,23 +1,60 @@
 package com.campee.starship;
 
 import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.Pixmap;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.ScreenUtils;
 
 public class MoonshipGame extends Game {
 	 Order orderScreen;
 	 TitleScreen titleScreen;
+	 SpriteBatch batch;
+	 Texture img;
 
 	@Override
 	public void create() {
+//		orderScreen = new Order();
+//		titleScreen = new TitleScreen();
+//		setScreen(orderScreen);
+//		try {
+//			Thread.sleep(5000);
+//		} catch (InterruptedException e) {
+//			e.printStackTrace();
+//		}
+//		setScreen((Screen) titleScreen);
+		batch = new SpriteBatch();
 		orderScreen = new Order();
 		titleScreen = new TitleScreen();
-		setScreen(orderScreen);
-		try {
-			Thread.sleep(5000);
-		} catch (InterruptedException e) {
-			e.printStackTrace();
+	}
+
+	public void render() {
+		//Gdx.gl.glClearColor(1, 1, 1, 1);
+		ScreenUtils.clear(Color.PURPLE);
+		//batch.begin();
+		//batch.draw(img, 400 - (img.getWidth() / 2), 300 - (img.getHeight() / 2));
+		//batch.end();
+		orderScreen.render();
+		//titleScreen.render();
+
+		if (orderScreen.press) {
+			ScreenUtils.clear(Color.BLUE);
+			orderScreen.dispose();
+			titleScreen.render();
 		}
-		setScreen((Screen) titleScreen);
+		//titleScreen.render();
+	}
+	public void setScreen() {
+		ScreenUtils.clear(Color.YELLOW);
+		System.out.println("you got here");
+	}
+
+	public void dispose() {
+		batch.dispose();
+		//img.dispose();
 	}
 
 	// Other methods in your game class
--- core/src/com/campee/starship/Order.java
+++ core/src/com/campee/starship/Order.java
@@ -1,31 +1,41 @@
 package com.campee.starship;
 
-import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.Screen;
-import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.InputListener;
+import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.utils.ScreenUtils;
 
-public class Order implements Screen {
+import java.util.ArrayList;
+import java.util.List;
+public class Order {
+    Boolean press = false;
     Stage stage;
-    public void show () {
+    TitleScreen titleScreen;
+    MoonshipGame game;
+    public Order () {
         stage = new Stage();
+        game = new MoonshipGame();
+        titleScreen = new TitleScreen();
         Gdx.input.setInputProcessor(stage);
+        //ScreenUtils.clear(1, 0.8f, 1, 1);
         BitmapFont font = new BitmapFont();
         TextButton.TextButtonStyle textButtonStyle = new TextButton.TextButtonStyle();
         textButtonStyle.font = font;
 
-        textButtonStyle.fontColor = Color.WHITE;
+        textButtonStyle.fontColor = Color.BLACK;
         TextButton button = new TextButton("poke the button!", textButtonStyle);
         button.setColor(Color.WHITE);
         button.setX(250);
         button.setY(250);
         button.addListener(new InputListener() {
             public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
+                game.setScreen();
+                press = true;
+
                 System.out.println("oop! you poked me!");
                 return true;
             }
@@ -35,39 +45,11 @@ public class Order implements Screen {
     }
 
     public void render() {
+        // stage.act(Gdx.graphics.getDeltaTime());
         stage.draw();
     }
 
-
-
-
-    @Override
-    public void render(float delta) {
-
-    }
-
-    @Override
-    public void resize(int width, int height) {
-
-    }
-
-    @Override
-    public void pause() {
-
-    }
-
-    @Override
-    public void resume() {
-
-    }
-
-    @Override
-    public void hide() {
-
-    }
-
-    @Override
     public void dispose() {
-
+        stage.dispose();
     }
 }
