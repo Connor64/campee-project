17855742fb74970d88aca965413f5f51e7a44689
evelyn <merze@purdue.edu>
2023-10-12 14:53:53 -0400

    edit title merged

--- core/src/com/campee/starship/MoonshipGame.java
+++ core/src/com/campee/starship/MoonshipGame.java
@@ -10,33 +10,20 @@ import com.badlogic.gdx.graphics.Pixmap;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
-import com.badlogic.gdx.utils.viewport.ExtendViewport;
 
 public class MoonshipGame extends Game {
-    //GameScreen gameScreen;
-<<<<<<< HEAD
+	//GameScreen gameScreen;
 	//private Order order;
 	private TitleScreen titleScreen;
-	//private ExtendViewport viewport;
 	//private TestScreen testScreen;
-=======
-	//private OrderScreen order;
-	//private TitleScreen titleScreen;
-	private TestScreen testScreen;
->>>>>>> 63cfb4d72ea7f3dde2ff0581f7c15107334544d4
 
 	@Override
 	public void create() {
-		//titleScreen = new TitleScreen(this);
-		testScreen = new TestScreen(this);
-		//order = new OrderScreen(this);
+		titleScreen = new TitleScreen(this);
+		//testScreen = new TestScreen(this);
+		//order = new Order(this);
 		//gameScreen = new GameScreen(this);
-<<<<<<< HEAD
-		//viewport = new ExtendViewport(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
 		setScreen(titleScreen);
-=======
-		setScreen(testScreen);
->>>>>>> 63cfb4d72ea7f3dde2ff0581f7c15107334544d4
 	}
 
 }
