62276767fe182767bb51350b9ca2b6c4a0c8ada5
Megan B <meganbechtlofft@gmail.com>
2023-10-13 14:25:58 -0400

    added back in the screen navigating

--- core/src/com/campee/starship/GameplayScreen.java
+++ core/src/com/campee/starship/GameplayScreen.java
@@ -72,7 +72,7 @@ public class GameplayScreen implements Screen {
             @Override
             public void clicked(InputEvent event, float x, float y) {
                 System.out.println("clicked back");
-                game.setScreen(new TitleScreen(game));
+                game.setScreen(new LevelScreen(game));
             }
         });
 
