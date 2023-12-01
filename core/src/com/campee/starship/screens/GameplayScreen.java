579d0f3b769d8356b879ea80355ff2c931c2be44
Emma <emmapatrick2003@gmail.com>
2023-12-01 00:45:31 -0500

    added tutorial and fixed fonts

--- core/src/com/campee/starship/screens/GameplayScreen.java
+++ core/src/com/campee/starship/screens/GameplayScreen.java
@@ -1007,6 +1007,7 @@ public class GameplayScreen extends ApplicationAdapter implements Screen {
         int timeMinutes = Integer.parseInt(scanner.nextLine());
         int timeSeconds = Integer.parseInt(scanner.nextLine());
         String name = scanner.nextLine();
+        scanner.nextLine();
         int minOrders = Integer.parseInt(scanner.nextLine());
         return minOrders;
     }
